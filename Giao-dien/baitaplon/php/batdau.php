ab08a7d287538de6b969d1e3b7dbb35dd011d475
NguyenVinhHung2705 <2705oniichan@gmail.com>
2025-05-01 17:29:18 +0700

    Xóa submodule, thêm lại Giao-dien như thư mục bình thường

---
+++ Giao-dien/baitaplon/php/batdau.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/batdau.css">
+    <link rel="stylesheet" href="../css/header_footer.css">
+    <script src="https://kit.fontawesome.com/bb6c8d9b87.js" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+    <?php
+		require_once("../../../ket-noi-co-so-du-lieu.php"); // ..(1) out ra php ..(2) out ra baitaplon ..(3) out ra Giao-dien
+		$sql = "select * from sanpham";
+		$kq = mysqli_query($conn, $sql); //Go
+	?>
+    <div class="container">
+        <div class="header">
+            <div class="logo">
+                <a href="batdau.php">
+                <i class="fa-solid fa-glass-water"></i>
+                <span>Milk</span>   
+                </a>
+            </div>
+            <div class="menu">
+                <div class="chose">
+                    <a href="batdau.php">Home</a>
+                </div>
+                <div class="chose">
+                    <a href="batdau.php">Sản Phẩm</a>
+                </div>
+                <div class="chose">
+                    <a href="batdau.php">Thông tin</a>
+                </div>
+                <div class="chose">
+                    <a href="batdau.php">Liên hệ</a>
+                </div>
+            </div>
+            <div class="user">
+                <a href="#">
+                    <i class="fa-solid fa-user"></i>
+                </a>
+            </div>
+            <div class="shopcart">
+                <a href="#">
+                    <i class="fa-solid fa-cart-shopping"></i>
+                    <div class="shopcart_value">1</div>
+                </a>
+            </div>
+        </div>
+        <div class="maincontent">
+            <div class="home">
+                <div class="home_content">
+                    <p>
+                        <span style="color: black;">Shop sữa tươi</span>
+                        <span style="color: #fff;">hàng trong ngày</span>
+                        <i>Trải nghiệm sự phong phú và tinh khiết của các sản phẩm sữa cao cấp của chúng tôi, được lấy trực tiếp từ các nhãn hàng uy tín đến tận nhà bạn.</i>    
+                    </p>
+                </div>
+                <div class="home_img">
+                    <img src="" alt="">
+                </div>
+            </div>
+            <div class="products">
+                <div class="products_head">
+                    <p>
+                        <span>Sản phẩm của chúng tôi</span>
+                        <i>Khám phá các sản phẩm từ sữa tươi của chúng tôi, được sản xuất cẩn thận từ trang trại đến bàn ăn.</i>
+                    </p>
+                </div>
+                <div class="products_maincontent">
+                    <input type="search" placeholder="Tìm kiếm các sản phảm ưng ý...">
+                    <div class="products_product">
+                        <?php
+                            while($row = mysqli_fetch_assoc($kq)){
+                        ?>
+                        <div class="product">
+                            <div class="product_img"><img src="../img/<?php echo $row['image'] ?>" alt="demo"></div>
+                            <div class="product_info">
+                               <p>
+                                    <span><?php echo $row["ten"] ?></span>
+                                    <span><?php echo $row["giaban"] ?></span>
+                                    <i>Mô tả</i>
+                                </p>
+                            </div>
+                            <div class="button_add_card">
+                                <button><i class="fa-solid fa-cart-shopping"></i><span> Thêm vào giỏ hàng</span></button>
+                            </div>
+                        </div>
+                        <?php
+                            }
+                            mysqli_close($conn);
+                        ?>
+                    </div>
+                </div>
+            </div>
+            <div class="about">
+                <div class="about_head">
+                    <p>
+                        <span>Về chúng tôi</span>
+                        <i>Với hơn 15 năm kinh nghiệm trong lĩnh vực phân phối sữa, chúng tôi mang đến cho bạn những sản phẩm sữa chất lượng tốt nhất trên thị trường với giá cả hợp lý dịch vụ tận tình nhất.</i>
+                    </p>
+                </div>
+                <div class="about_contents">
+                    <div class="about_contents_img">
+                        <img src="" alt="">
+                    </div>
+                    <div class="about_contents_content">
+                            <span>Triết lý từ trang trại đến bàn ăn</span>
+                            <i>Tại Milk, chúng tôi tin vào tầm quan trọng của việc biết thực phẩm của bạn đến từ đâu. Triết lý từ nông trại đến bàn ăn của chúng tôi đảm bảo rằng từng giọt sữa đến tay bạn ở dạng tinh khiết nhất, giữ nguyên mọi dưỡng chất và lợi ích tự nhiên.</i>
+                            <i>Chúng tôi làm việc trực tiếp với những người nông dân địa phương có chung niềm đam mê với các hoạt động canh tác có đạo đức và phúc lợi động vật, tạo ra một hệ sinh thái bền vững mang lại lợi ích cho tất cả mọi người, từ người nông dân đến người tiêu dùng.</i>
+                    </div>
+                </div>
+                <div class="about_spe">
+                    <div class="about_spe_impo">
+                            <h2><i class="fa-solid fa-glass-water"></i></h2>
+                            <span>Chất lượng của từng trang trại,nguồn hàng</span>
+                            <i>Sữa của chúng tôi được lấy trực tiếp từ các nguồn hàng với trang trại an toàn và những chú bò ăn cỏ hạnh phúc.</i>
+                    </div>
+                    <div class="about_spe_impo">
+                            <h2><i class="fa-regular fa-circle-check"></i></h2>
+                            <span>100% đảm bảo nguồn hàng chất lượng, Tự nhiên</span>
+                            <i>Không có chất bảo quản hoặc phụ gia, chỉ có sữa tự nhiên nguyên chất</i>
+                    </div>
+                    <div class="about_spe_impo">
+                            <h2><i class="fa-solid fa-box"></i></h2>
+                            <span>Giao hàng nhanh chóng, tiện lợi và đảm bảo</span>
+                            <i>Sản phẩm tươi ngon được giao đến tận nhà bạn nhanh chóng với dịch vụ tận tâm và tiện lợi.</i>
+                    </div>
+                </div>
+            </div>
+            <div class="contract">
+                <div class="contract_head">
+                    <span>Liên hệ với chúng tôi</span>
+                    <i>Bạn có thắc mắc hoặc có ý kiến đóng góp? <br>Hãy liên hệ với chúng tôi và chúng tôi sẽ sẵng lòng hỗ trợ.</i>
+                </div>
+                <div class="askandinfo">
+                    <div class="askandinfo_form">
+                        <form action="info_user">
+                            <label for="txtName">Tên</label>
+                            <input type="text" name="txtName" placeholder="Họ và tên">
+                            <label for="txtEmail">Email</label>
+                            <input type="email" name="txtEmail" placeholder="Email@gmail.com">
+                            <label for="txtMessage">Lời nhắn</label>
+                            <input type="text" name="txtMessage" placeholder="lời nhắn">
+                            <button>Gửi</button>
+                        </form>
+                    </div>
+                    <div class="askandifo_info">
+                        <h2>Liên hệ với chúng tôi</h2>
+                        <p>
+                            <h3><i class="fa-solid fa-location-dot"></i> Địa chỉ</h3>
+                            <span>XX Khuê Trung, Cẩm Lệ, TP.Đà Nẵng</span>
+                        </p>
+                        <p>
+                            <h3><i class="fa-solid fa-envelope"></i> Email</h3>
+                            <span>honguyenkhoi2710@gmail.com</span>
+                        </p>
+                        <p>
+                            <h3><i class="fa-solid fa-phone"></i> Phone</h3>
+                            <span>-0867601269</span>
+                        </p>
+                        <h3><span>Chúng tôi luôn sẵng sàng 24/24</span></h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer">
+            <div class="footer_content">
+                <div class="footer_menu">
+                    <div class="footer_menu_st">
+                        <h1><i class="fa-solid fa-glass-water"></i> MILK</h1>
+                        <span>Các sản phẩm từ sữa cao cấp được giao tươi đến tận nhà bạn mỗi ngày.</span>
+                    </div>
+                    <div class="footer_menu_st">
+                        <h2>Qick Links</h2>
+                        <span><a href="#">Home</a></span>
+                        <span><a href="#">Sản phẩm</a></span>
+                        <span><a href="#">Thông tin</a></span>
+                        <span><a href="#">Liên hệ</a></span>
+                    </div>
+                    <div class="footer_menu_st">
+                        <h2>Các nhãn hàng</h2>
+                        <span><a href="#">Vinamilk</a></span>
+                        <span><a href="#">TH true MILK</a></span>
+                        <span><a href="#">Nutifood</a></span>
+                        <span><a href="#">Dutch Lady</a></span>
+                    </div>
+                    <div class="footer_menu_st">
+                        <h2>Hãy là một thành viên của chúng tôi!</h2>
+                        <span><a href="#">tham gia Tuyển dụng ngay</a></span>
+                    </div>
+                </div>
+                <div class="footer_end">
+                    <hr>
+                    <h6>© 2025 Milk. All rights reserved.</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ Giao-dien/product.sql
@@ -0,0 +1,129 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 01, 2025 at 12:17 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `product`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `khachhang`
+--
+
+CREATE TABLE `khachhang` (
+  `id` int(11) NOT NULL,
+  `ten_dang_nhap` varchar(255) NOT NULL,
+  `ho_ten` varchar(255) NOT NULL,
+  `gioi_tinh` varchar(5) NOT NULL,
+  `dia_chi` varchar(255) NOT NULL,
+  `so_dien_thoai` varchar(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `mat_khau` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `khachhang`
+--
+
+INSERT INTO `khachhang` (`id`, `ten_dang_nhap`, `ho_ten`, `gioi_tinh`, `dia_chi`, `so_dien_thoai`, `email`, `mat_khau`) VALUES
+(3, 'demo', 'demo', 'Nữ', 'demo', 'demo', 'demo@gmail.com', 'demo'),
+(4, 'admin', 'admin', '', 'admin', '', 'admin', 'admin'),
+(5, 'test', 'test', 'Nữ', 'test', '0123456789', 'test@gmail.com', 'ahihi');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sanpham`
+--
+
+CREATE TABLE `sanpham` (
+  `id` int(11) NOT NULL,
+  `ten` varchar(255) NOT NULL,
+  `nhanhang` varchar(225) NOT NULL,
+  `giaban` int(11) NOT NULL,
+  `image` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `sanpham`
+--
+
+INSERT INTO `sanpham` (`id`, `ten`, `nhanhang`, `giaban`, `image`) VALUES
+(47, 'Sữa tươi tiệt trùng 100% Vinamilk 180ml (48 hộp/thùng)', 'Vinamilk', 422081, 'Sua_tuoi_tiet_trung_100%_Vinamilk_180ml.png'),
+(48, 'Sữa bột Dielac Grow Plus 1+ 800g', 'Vinamilk', 412854, 'Sua_bot_Dielac_Grow_Plus_1+_800g.png'),
+(49, 'Sữa bột Optimum Gold 2 800g', 'Vinamilk', 419000, 'Sua_bot_Optimum_Gold_2_800g.png'),
+(50, 'Sữa bột Optimum Gold 3 850g', 'Vinamilk', 419000, 'Sua_bot_Optimum_Gold_3_850g.png'),
+(51, 'Sữa uống dinh dưỡng Yoko Gold 110ml (lốc 4 hộp)', 'Vinamilk', 120000, 'Sua_uong_dinh_duong_Yoko_Gold_110ml.png'),
+(52, 'Sữa uống dinh dưỡng Yoko Gold 180ml (lốc 4 hộp)', 'Vinamilk', 189000, 'Sua_uong_dinh_duong_Yoko_Gold_180ml.png'),
+(53, 'Sữa tươi tiệt trùng TH true MILK 110ml (48 hộp/thùng)', 'TH true MILK', 298800, 'Sua_tuoi_tiet_trung_TH_true_MILK_110ml.png'),
+(54, 'Sữa tươi tiệt trùng TH true MILK 180ml (48 hộp/thùng)', 'TH true MILK', 469200, 'Sua_tuoi_tiet_trung_TH_true_MILK_180ml.png'),
+(55, 'Sữa tươi tiệt trùng TH true MILK ít đường 180ml (lốc 4 hộp)', 'TH true MILK', 39100, 'Sua_tuoi_tiet_trung_TH_true_MILK_it_duong_180ml.png'),
+(56, 'Sữa công thức TH true FORMULA 110ml (lốc 4 hộp)', 'TH true MILK', 47520, 'Sua_cong_thuc_TH_true_FORMULA_110ml.png'),
+(57, 'Sữa công thức TH true FORMULA 180ml (lốc 4 hộp)', 'TH true MILK', 69908, 'Sua_cong_thuc_TH_true_FORMULA_180ml.png'),
+(58, 'Sữa tươi tiệt trùng TH true MILK có đường 110ml (lốc 4 hộp)', 'TH true MILK', 24900, 'Sua_tuoi_tiet_trung_TH_true_MILK_co_duong_110ml.png'),
+(59, 'Sữa dinh dưỡng pha sẵn Nuvi Grow 2+ 110ml (lốc 4 hộp)', 'Nutifood', 28000, 'Sua_dinh_duong_pha_san_Nuvi_Grow_2+_110ml.png'),
+(60, 'Sữa dinh dưỡng pha sẵn Nuvi Grow 1+ 110ml (lốc 4 hộp)', 'Nutifood', 28000, 'Sua_dinh_duong_pha_san_Nuvi_Grow_1+_110ml.png'),
+(61, 'Sữa bột GrowPLUS+ Boosting Digestion 2+ 800g', 'Nutifood', 475000, 'Sua_bot_GrowPLUS+_Boosting_Digestion_2+_800g.png'),
+(62, 'Sữa bột GrowPLUS+ Boosting Digestion 1+ 800g', 'Nutifood', 495000, 'Sua_bot_GrowPLUS+_Boosting_Digestion_1+_800g.png'),
+(63, 'Sữa tươi tiệt trùng Dutch Lady có đường 110ml (48 hộp/thùng)', 'Dutch Lady', 215000, 'Sua_tuoi_tiet_trung_Dutch_Lady_co_duong_110ml.png'),
+(64, 'Sữa tươi tiệt trùng Dutch Lady có đường 180ml (48 hộp/thùng)', 'Dutch Lady', 320000, 'Sua_tuoi_tiet_trung_Dutch_Lady_co_duong_180ml.png'),
+(65, 'Sữa tươi tiệt trùng Dutch Lady không đường 180ml (48 hộp/thùng)', 'Dutch Lady', 320000, 'Sua_tuoi_tiet_trung_Dutch_Lady_khong_duong_180ml.png'),
+(66, 'Sữa tươi tiệt trùng Dutch Lady có đường 220ml (48 bịch/thùng)', 'Dutch Lady', 320000, 'Sua_tuoi_tiet_trung_Dutch_Lady_co_duong_220ml.png'),
+(67, 'Sữa tươi tiệt trùng Dutch Lady không đường 220ml (48 bịch/thùng)', 'Dutch Lady', 320000, 'Sua_tuoi_tiet_trung_Dutch_Lady_khong_duong_220ml.png'),
+(68, 'Sữa tươi tiệt trùng Dutch Lady có đường 1 lít (12 hộp/thùng)', 'Dutch Lady', 360000, 'Sua_tuoi_tiet_trung_Dutch_Lady_co_duong_1_lit.png'),
+(69, 'Sữa tươi tiệt trùng Dutch Lady không đường 1 lít (12 hộp/thùng)', 'Dutch Lady', 360000, 'Sua_tuoi_tiet_trung_Dutch_Lady_khong_duong_1_lit.png');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `khachhang`
+--
+ALTER TABLE `khachhang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sanpham`
+--
+ALTER TABLE `sanpham`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`,`ten`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `khachhang`
+--
+ALTER TABLE `khachhang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `sanpham`
+--
+ALTER TABLE `sanpham`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
