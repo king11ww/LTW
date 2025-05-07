8b008209c4fc38a732ba04606f940d727730cfd6
king11ww <honguyenkhoi2710@gmail.com>
2025-05-07 12:00:44 +0700

    Lưu thay đổi trước khi merge

--- Giao-dien/baitaplon/php/thanhtoan.php
+++ Giao-dien/baitaplon/php/thanhtoan.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
     session_start();
 
@@ -50,16 +49,4 @@
         <a href="batdau.php">Quay lại trang chủ</a>
     </div>
 </body>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
->>>>>>> 51c1a6f163f2cd4855c5ee8f17d1436d99f2e330
 </html>
